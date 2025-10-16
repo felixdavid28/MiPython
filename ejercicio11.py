ba492e6a12e547e5551b207b8aaa3de35e61eb66
Felix David <fromlop1311@zaidinvergeles.org>
2025-10-16 10:45:58 +0200

    ejercicio8.py

---
+++ ejercicio11.py
@@ -0,0 +1,6 @@
+cantidad= input("dime la cantidad de dinero a ingresar")
+interes= int(cantidad)*0.4
+total1= ((int(cantidad)*int(interes)*1)/100)
+total2= ((int(total1)*int(interes)*1)/100)
+total3= ((int(total2)*int(interes)*1)/100)
+print("El primer año de tu interes es:{total1}" )
---
+++ ejercicio8.py
@@ -0,0 +1,7 @@
+# Solicitar dos números enteros al usuario
+n1 = int(input("Introduce el primer número entero (n): "))
+n2 = int(input("Introduce el segundo número entero (m): "))
+c = n1 / n2 
+r = n1 % n2   
+# Mostrar el resultado
+print(f"{n1} entre {n2} da un cociente {c} y un resto {r}")
