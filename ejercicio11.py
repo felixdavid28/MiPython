2b93eb951fc0ffd9df0f63c465b964e3776b6fce
Felix David <fromlop1311@zaidinvergeles.org>
2025-10-23 09:48:43 +0200

    ejercicio11.py

--- ejercicio11.py
+++ ejercicio11.py
@@ -1,6 +1,10 @@
-cantidad= input("dime la cantidad de dinero a ingresar")
-interes= int(cantidad)*0.4
-total1= ((int(cantidad)*int(interes)*1)/100)
-total2= ((int(total1)*int(interes)*1)/100)
-total3= ((int(total2)*int(interes)*1)/100)
-print("El primer año de tu interes es:{total1}" )
+
+deposito_inicial = float(input("Introduce la cantidad de dinero depositada: "))
+interes = 0.04
+ahorro_1 = deposito_inicial * (1 + interes)
+ahorro_2 = ahorro_1 * (1 + interes)
+ahorro_3 = ahorro_2 * (1 + interes)
+
+print(f"Ahorros después del primer año: {ahorro_1:.2f} €")
+print(f"Ahorros después del segundo año: {ahorro_2:.2f} €")
+print(f"Ahorros después del tercer año: {ahorro_3:.2f} €")
