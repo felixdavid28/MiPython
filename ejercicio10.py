4014a176261a7f2ee6af590d4fb96cbd9da28c62
Felix David <fromlop1311@zaidinvergeles.org>
2025-10-23 10:15:53 +0200

    ejercicio12.py

---
+++ ejercicio10.py
@@ -0,0 +1,7 @@
+payasos = float(input("numero de payasos: "))
+munecas = float(input("numero de muñecas: "))
+
+Rpayasos = payasos*112
+Rmunecas = munecas*75
+resultado= Rpayasos + Rmunecas
+print("El total es",resultado)
---
+++ ejercicio12.py
@@ -0,0 +1,4 @@
+pan = int(input("Introduce la cantidad de pan vendido que no es del dia: "))
+precio= pan*3.49
+descuento= precio*0.60
+print(f"El total con el descuento aplicado es:{descuento:.3f} €")
